eb6524d962994a5173b72a3ff565b2e991797222
aze6809 <71939491+aze6809@users.noreply.github.com>
2022-11-20 00:23:36 -0500

    Can add new entries to database via form on home page!
    Planning to move form to seperate page...

--- index.php
+++ index.php
@@ -1,32 +1,78 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
+  <meta charset="utf-8">
   <link rel="stylesheet" href="style.css">
   <title>Subtitle Language Search </title>
 </head>
 <body>
   <header> <h1>Find a subtitle language today!</h1>
-<nav><a href="index.php" style="padding-right: 25px; ">Database</a> <a href="about.html">About</a></nav>
-</header>
+    <nav> <a href="index.php" style="padding-right: 25px; ">Database</a> <a href="about.html">About</a> </nav>
+  </header>
 
-  <form>
-    <label for="search">Enter the name of a film or show:</label>
-    <input type="text" id="search" name="search" placeholder="Search.." cursor: pointer;>
-    <button type="submit" action="index.php" method="post">Search</button>
-  </form>
-  <p> Showing results for: </p>
-  <?php 
-  //echo $_GET["search"];
-  if(!empty($_GET)){
-    $searchPhrase = $_GET["search"];
-    $sql = 'SELECT * from `mediaDatabase` WHERE title IN ($searchPhrase)';
-    echo $sql;
+  <div class="new_media"> 
+    <form action="index.php" method="get"> Have a show/movie to submit? Enter the data and add to the database? <br/>
+      <!--Name to be entered into table -->
+      <label>Name of TV show or movie:</label> <input type="text" id="name" name="name"> </input><br/><br/>
+      <!--Comment to be entered into table -->
+      <label>Languages:</label> <input type="text" id="langs" name="langs">  </input>
+      <br/><br/>
+      <label>Platform:</label> <input type="text" id="plat" name="plat">  </input>
+      <br/><br/>
+      <input type="submit" value="Add to the List">
+    </form>
+<?php 
+    include "../../dbCon.php";
+  if ($mysqli) {
+    //if the name and comment fields aren't empty, then do the next thing
+    if( !empty($_GET['name']) && !empty($_GET['langs']) && !empty($_GET['plat'])){
+      //inserts the input into the table
+      $stmt=$mysqli->prepare("insert into mediaDatabase (title, languages, platforms) values (?, ?, ?)");
+      $stmt->bind_param("sss",$_GET['name'],$_GET['langs'], $_GET['plat']);
+      $stmt->execute();
+      $stmt->close();
+    }
+    $res=$mysqli->query('select name, langs, plat from mediaDatabase');
+    if($res){
+      while($rowHolder = mysqli_fetch_array($res,MYSQLI_ASSOC)){
+        $records[] = $rowHolder;
+      }
+    }
   }
-  ?>
+
 
   
-</div>
+?>
+  </div>
+  
+<div class="filter">
+    <form>
+      <input type="radio" name="services" value="services">
+				<label for="netflix">Netflix</label><br>
+				<input type="radio" name="services" value="hulu">
+				<label for="hulu">Hulu</label><br>
+
+				<p>
+					<input type="submit"  name="Submit"  value="Send_Form" action="index.php" method="post" />
+				</p>
+    </form>
+
+    <?php
+      include "../../dbCon.php";
+
+
+      /*
+      $streaming = $_GET['services'];
+    
+      switch ($streaming){
+        case 'hulu':
+          echo 'Hulu results: ';
+          $sql = 'SELECT * FROM `mediaDatabase` WHERE platforms =Hulu';
+          break; 
+      }
+*/
+    ?>
+  </div>
 
   <div class="media_database">
     
@@ -53,20 +99,13 @@
       $things = array();
       
 
-      //$things = array_keys($records);
-      //print_r($records);
-     
       foreach($records as $name=>$value){
-       
           
           $extra_output = '<div class="movie_entry"> <h3>Title: ' . $value['title'] . '</h3> <h4>Platform: ' .$value['platforms']. '</h4><br> <h4>Languages</h4><br> <p> ' .
            $value['languages'] . '</p> </div>' ;
-          //things[2]
+          
         echo $extra_output;
     }
-   
-     
-      
 
 ?>
 
